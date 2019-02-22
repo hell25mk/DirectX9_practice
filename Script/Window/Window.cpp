ae79f2b78443f5dd6078662ef1972e19967621fe
hell25mk <hell25mk@gmail.com>
2019-02-22 20:54:42 +0900

    [update] Windowクラスの更新
    DirectXで扱いやすいように変更

--- Script/Window/Window.cpp
+++ Script/Window/Window.cpp
@@ -1,117 +1,105 @@
 #include "Window.h"
 
-/*Window::Window():widthSize(640), heightSize(480){
-}
+Window::Window()
+	:widthSize(640), heightSize(480), nCmdShow(SW_SHOW){
 
-void Window::SetWindowSize(const int wid, const int hig){
-
-	widthSize = wid;
-	heightSize = hig;
+	hInstance = GetModuleHandle(NULL);
+	SetWindowName(Default_WindowName);
 
 }
 
-void Window::SetClassName(_TCHAR* const name){
+Window::~Window(){
 
-	className = name;
+	DestroyWindow(hWnd);		//Windowの破棄
+	MessageBox(NULL, _T("デストローイ"), _T("メッセージ"), MB_OK);
 
 }
 
-void Window::SetWindowName(_TCHAR* const name){
+/// <summary>
+/// @brief ウィンドウを生成するクラス
+/// </summary>
+/// <returns>true 生成</returns>
+/// <returns>false 生成失敗</returns>
+bool Window::WindowInit(){
 
-	windowName = name;
+#pragma region どのようなウィンドウを作成するか
+	wc.cbClsExtra = 0;											//構造体のサイズを拡張するバイト数
+	wc.cbWndExtra = 0;											//ウィンドウのサイズを拡張するバイト数
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);		//ウィンドウの背景を塗りつぶすブラシ
+	wc.hCursor = LoadCursor(hInstance, IDC_ARROW);				//ウィンドウ上にマウスカーソルが来た場合のマウスの形状
+	wc.hIcon = NULL;											//ウィンドウのアイコン（LoadImage()でつけてもよい）
+	wc.hInstance = hInstance;									//ウィンドウを作るためのインスタンスハンドルを指定
+	wc.lpfnWndProc = WndProc;									//ウィンドウから通知されたイベントを処理するための関数を指定
+	wc.lpszClassName = _T("WindowsAPI");						//登録するウィンドウのクラス名
+	wc.lpszMenuName = NULL;										//メニューのリソースID
+	wc.style = NULL;											//ウィンドウクラスのスタイル
 
-}*/
+	//ウィンドウの登録に失敗した場合終了させる
+	if(!RegisterClass(&wc)){
+		MessageBox(NULL, _T("ウィンドウの登録に失敗しました"), _T("エラー"), MB_OK);
+		return false;
+	}
+#pragma endregion
 
-///// <summary>
-///// @brief ウィンドウイベントを処理する
-///// </summary>
-///// <param name="hWnd">イベントが発生したウィンドウハンドル</param>
-///// <param name="msg">発生したイベント内容</param>
-///// <param name="wParam">イベントから通知されたデータ</param>
-///// <param name="lParam">イベントから通知されたデータ</param>
-///// <returns></returns>
-//LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam){
-//
-//	//イベント処理
-//	switch(msg){
-//		case WM_DESTROY:
-//			//ウィンドウが閉じられたらプログラムを終了する
-//			PostQuitMessage(NULL);
-//			break;
-//		default:
-//			//システムにイベント処理を投げる
-//			return (DefWindowProc(hWnd, msg, wParam, lParam));
-//	}
-//
-//	return 0;
-//}
-//
-//bool Window::OriginalCreateWindow(){
-//
-//	HINSTANCE hInstance = GetModuleHandle(NULL);		//インスタンスハンドルを取得
-//
-//	WNDCLASS wc;		//登録する情報を格納する構造体
-//
-//#pragma region どのようなウィンドウを作成するか
-//	wc.cbClsExtra = 0;											//構造体のサイズを拡張するバイト数
-//	wc.cbWndExtra = 0;											//ウィンドウのサイズを拡張するバイト数
-//	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);		//ウィンドウの背景を塗りつぶすブラシ
-//	wc.hCursor = LoadCursor(hInstance, IDC_ARROW);				//ウィンドウ上にマウスカーソルが来た場合のマウスの形状
-//	wc.hIcon = NULL;											//ウィンドウのアイコン（LoadImage()でつけてもよい）
-//	wc.hInstance = hInstance;									//ウィンドウを作るためのインスタンスハンドルを指定
-//	wc.lpfnWndProc = WndProc;								//ウィンドウから通知されたイベントを処理するための関数を指定
-//	wc.lpszClassName = className;								//登録するウィンドウのクラス名
-//	wc.lpszMenuName = NULL;										//メニューのリソースID
-//	wc.style = NULL;											//ウィンドウクラスのスタイル
-//
-//	//ウィンドウの登録に失敗した場合終了させる
-//	if(RegisterClass(&wc) == NULL){
-//		return false;
-//	}
-//#pragma endregion
-//
-//#pragma region ウィンドウの作成
-//	HWND hWnd = CreateWindow(
-//		wc.lpszClassName,							//登録されているクラス名
-//		windowName,									//ウィンドウのタイトル
-//		(WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX),	//ウィンドウのスタイル(タイトル、システム、最小化使用)
-//		CW_USEDEFAULT,								//ウィンドウのx位置(システム標準値を使用)
-//		CW_USEDEFAULT,								//ウィンドウのy位置
-//		widthSize,									//ウィンドウの幅
-//		heightSize,									//ウィンドウの高さ
-//		NULL,										//親ウィンドウのハンドル
-//		NULL,										//メニューハンドル
-//		hInstance,
-//		NULL,										//ウィンドウ作成時のイベントへ渡すデータのポインタ
-//		);
-//
-//	//ウィンドウサイズの調整
-//	RECT r1, r2;
-//
-//	GetClientRect(hWnd, &r1);
-//	GetWindowRect(hWnd, &r2);
-//	MoveWindow(
-//		hWnd, r2.left, r2.top,
-//		widthSize + ((r2.right - r2.left) - (r1.right - r1.left)),
-//		heightSize + ((r2.bottom - r2.top) - (r1.bottom - r1.top)),
-//		false
-//	);
-//#pragma endregion
-//
-//}
-//
-//bool Window::ProcessLoop(){
-//
-//	MSG msg;
-//
-//	if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
-//		if(msg.message == WM_QUIT){
-//			return false;
-//		}
-//
-//		TranslateMessage(&msg);
-//		DispatchMessage(&msg);
-//	}
-//
-//	return true;
-//}
+#pragma region ウィンドウの作成
+	hWnd = CreateWindow(
+		wc.lpszClassName,								//登録されているクラス名
+		windowName,										//ウィンドウのタイトル
+		(WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX),		//ウィンドウのスタイル(タイトル、システム、最小化使用)
+		CW_USEDEFAULT,									//ウィンドウのx位置(システム標準値を使用)
+		CW_USEDEFAULT,									//ウィンドウのy位置
+		widthSize,										//ウィンドウの幅
+		heightSize,										//ウィンドウの高さ
+		NULL,											//親ウィンドウのハンドル
+		NULL,											//メニューハンドル
+		hInstance,
+		NULL											//ウィンドウ作成時のイベントへ渡すデータのポインタ
+		);
+
+	if(!hWnd){
+		MessageBox(NULL, _T("ウィンドウの生成に失敗しました"), _T("エラー"), MB_OK);
+		return false;
+	}
+
+	//ウィンドウサイズの調整
+	RECT r1, r2;
+
+	GetClientRect(hWnd, &r1);
+	GetWindowRect(hWnd, &r2);
+	MoveWindow(
+		hWnd, r2.left, r2.top,
+		widthSize + ((r2.right - r2.left) - (r1.right - r1.left)),
+		heightSize + ((r2.bottom - r2.top) - (r1.bottom - r1.top)),
+		false
+	);
+#pragma endregion
+
+	//ウィンドウの表示
+	ShowWindow(hWnd, nCmdShow);
+
+	MessageBox(NULL, _T("ショーウインドー"), _T("メッセージ"), MB_OK);
+
+	return true;
+}
+
+/// <summary>
+/// @brief ウィンドウの処理を行う
+/// </summary>
+/// <returns>true それ以外</returns>
+/// <returns>false QUITメッセージ</returns>
+bool Window::Process() const{
+
+	MSG msg;
+
+	//ウィンドウに通達されようとしているイベントを取得
+	if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
+		if(msg.message == WM_QUIT){
+			return false;
+		}
+
+		TranslateMessage(&msg);	//仮想キーメッセージを文字メッセージへ変換
+		DispatchMessage(&msg);	//ウィンドウイベントを通達
+	}
+
+	return true;
+}
--- Script/Window/Window.h
+++ Script/Window/Window.h
@@ -3,25 +3,40 @@
 #include <Windows.h>
 #include <tchar.h>
 
-/*class Window final{
+namespace{
+	constexpr int Max_NameSize = 16;
+
+	LPCTSTR Default_WindowName = _T("MyWindow");
+}
+
+class Window{
 
 public:
 	Window();
-	~Window() = default;
-	
-	bool OriginalCreateWindow();
-	bool ProcessLoop();
+	~Window();			//ポリモーフィズムは使用しないためvirtualはつけていない
 
-	void SetWindowSize(const int wid, const int hig);
-	void SetClassName(_TCHAR* const name);
-	void SetWindowName(_TCHAR* const name);
+	bool WindowInit();
+	bool Process() const;
 
-private:
+#pragma region Accessor
+	bool SetWindowSize(const int wid, const int hig);
+	bool SetScreenMode(const bool flg);
+	bool SetWindowName(const TCHAR* name);
+	bool SetWindowIcon();
+	bool SetBackgroundColor(const int color);
+	bool SetBackgroundColor(const int r, const int g, const int b);
+#pragma endregion
+
+protected:
+	WNDCLASS wc;			//登録する情報を格納する構造体
+	HINSTANCE hInstance;	//インスタンスハンドルを取得
+	HWND hWnd;
 	int widthSize;
 	int heightSize;
-	_TCHAR* className;
-	_TCHAR* windowName;
+	bool isMaxWindowSize;
+	int nCmdShow;
+	TCHAR windowName[Max_NameSize];
 
 };
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);*/
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
